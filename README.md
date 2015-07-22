345dbfe32dd5af87950902221ebe84aa25291b81
Benedikt Bär <beniwtv@synergicbee.com>
2015-07-22 12:35:21 +0200

    - Added GitLab service - Added GitHub service
    (only login/logut for now)

--- README.md
+++ README.md
@@ -13,11 +13,28 @@ using Git-Deployer.
 To install Git-Deployer, you can download an [PHAR-archive][1], and put it
 somewhere in your $PATH, for example:
 
-  curl -o /usr/bin/git-deployer https://github.com/git-deployer
+```
+curl -o /usr/bin/git-deployer https://github.com/git-deployer
+```
 
 2) Using Git-Deployer
 -------------------------------------
 
-See git-deployer -h
+First, you will need to log-in to a Git service, like GitLab or GitHub. To
+know which services are available to you currently, use: 
+
+```
+git-deployer help login
+```
+
+This will list all services that are currently available in git-deployer. When
+you have chosen a service, log in to it with the command:
+
+```
+git-deployer login <service>
+```
+
+The service may ask you a few questions, like the log-in user and password.
+Now, see git-deployer -h for more commands!
 
 [1]: https://github.com/git-deployer
---
+++ src/GitDeployer/Commands/LogoutCommand.php
@@ -0,0 +1,36 @@
+<?php
+namespace GitDeployer\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class LogoutCommand extends Command {
+    
+    protected function configure() {
+
+        // Get available services to display to the user
+        $services = \GitDeployer\Services\BaseService::getServicesForHelp();
+
+        $this
+            ->setName('logout')
+            ->setDescription('Log out of the Git service (if logged in)');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        
+        // -> We check if there is already an app instance, and use that
+        // if it exists to log-in or pre-populate data
+        try {
+            $instance = \GitDeployer\AppInstance::getInstance();
+            $instance->delete();
+        } catch(\Exception $e) {
+            // No app instance? We're done here.
+        }
+
+        $output->writeln('You have successfully logged out</info>');
+
+    }
+
+}
