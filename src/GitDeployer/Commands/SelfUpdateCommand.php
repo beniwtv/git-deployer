73059f21e3482b3afeff72cce6b8f73e4dba8b57
Benedikt Bär <beniwtv@relamp.tk>
2015-10-25 13:37:53 +0100

    Working on the update command (WIP)

---
+++ src/GitDeployer/Commands/SelfUpdateCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace GitDeployer\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Humbug\SelfUpdate\Updater;
+
+class SelfUpdateCommand extends Command {
+    
+    protected function configure() {
+
+        $this
+            ->setName('self-update')
+            ->setDescription('Updates Git-Deployer to the latest version');
+
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+
+        $updater = new Updater();
+        $updater->setStrategy(Updater::STRATEGY_GITHUB);
+        $updater->getStrategy()->setPackageName('beniwtv/git-deployer');
+        $updater->getStrategy()->setPharName('git-deployer.phar');
+        $updater->getStrategy()->setCurrentLocalVersion($this->getApplication()->getVersion());
+
+        $result = $updater->update();
+
+        if ($result) { 
+            $new = $updater->getNewVersion();
+            $output->writeln('<info>Git-Deployer</info> updated to version '. $new . '.');
+        } else {
+            $output->writeln('<info>Git-Deployer</info> is already on the latest version!');
+        }
+
+    }
+
+}
